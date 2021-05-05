05bcf6b46b350f8ddd77c86031eb8e95e76dac8f
gregkristian <gkristianp@gmail.com>
2021-05-05 21:54:24 +0000

    Basic SQL, read to html

--- server/__init__.py
+++ server/__init__.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_restful import Resource, Api, reqparse
-from .endpoint import *
+from endpoint import *
+import sqlite3
 #import pandas as pd
 
 app = Flask(__name__)
@@ -8,18 +9,25 @@ api = Api(app)
 
 # API endpoints. Example:
 # http://URL:port/device?deviceId=2&visitorCount=100
-api.add_resource(GetStatus, '/getstatus')
+api.add_resource(GetStatus, '/api/getstatus')
 
 # Device endpoints
-api.add_resource(Device, '/device')
+api.add_resource(Device, '/api/device')
 
 # Mobile app endpoints
-api.add_resource(RegisterBusiness, '/registerbusiness')
-api.add_resource(Customer, '/customer')
+api.add_resource(RegisterBusiness, '/api/registerbusiness')
+api.add_resource(Customer, '/api/customer')
+
+#@app.route("/")
+#def not_found():
+    #return '404 Nothing here'
 
 @app.route("/")
-def not_found():
-    return '404 Nothing here'
+def home():
+    con = sqlite3.connect('example.db')
+    cur = con.cursor()
+    rows = cur.execute('SELECT * FROM stocks ORDER BY price')
+    return render_template('home.html', rows = rows)
 
 if __name__ == '__main__':
     app.run()
